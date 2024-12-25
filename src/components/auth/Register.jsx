41d8aa0a1abc541691aa2ba7fc0e067914f73552
Ivo Kostadinov <ivaylo.kostadinov98@gmail.com>
2024-12-25 09:36:47 +0200

    Finished Register component.

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -9,87 +9,90 @@ export default function Register() {
   }
 
   return (
-    <article className={styles.registerBox}>
-      <div className={styles.registerIntroBox}>
-        <img
-          className={styles.logo}
-          src="/logo.png"
-          alt="A logo consisting of a shield with a heart in the middle of it."
-        />
-        <header className={styles.title}>Join our supportive community</header>
-        {/* <p className={styles.subTitle}>Get Started!</p> */}
-      </div>
-      <form className={styles.contentGrid} onSubmit={handleSubmit}>
-        <div className={styles.formInputs}>
-          <input
-            className={styles.input}
-            type="text"
-            placeholder="Enter your unique username..."
+    <div className={styles.box}>
+      <article className={styles.registerBox}>
+        <div className={styles.registerIntroBox}>
+          <img
+            className={styles.logo}
+            src="/logo.png"
+            alt="A logo consisting of a shield with a heart in the middle of it."
           />
-          <input
-            className={styles.input}
-            type="email"
-            placeholder="Enter your email..."
-          />
-          <div className={styles.nameBox}>
-            <div className={styles.nameInputBox}>
+          <header className={styles.title}>
+            Join our supportive community
+          </header>
+        </div>
+        <form className={styles.contentGrid} onSubmit={handleSubmit}>
+          <div className={styles.formInputs}>
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="Enter your unique username..."
+            />
+            <input
+              className={styles.input}
+              type="email"
+              placeholder="Enter your email..."
+            />
+            <div className={styles.nameBox}>
+              <div className={styles.nameInputBox}>
+                <input
+                  className={styles.input}
+                  type="text"
+                  placeholder="Enter your name..."
+                />
+                <span className={styles.question}>?</span>
+              </div>
+              <p>(Optional)</p>
+            </div>
+            <div className={styles.passBox}>
               <input
                 className={styles.input}
-                type="text"
-                placeholder="Enter your name..."
+                type="password"
+                placeholder="Enter your password..."
               />
               <span className={styles.question}>?</span>
             </div>
-            <p>(Optional)</p>
+            <div className={styles.passBox}>
+              <input
+                className={styles.input}
+                type="password"
+                placeholder="Re-enter your password..."
+              />
+              <span className={styles.question}>?</span>
+            </div>
           </div>
-          <div className={styles.passBox}>
-            <input
-              className={styles.input}
-              type="password"
-              placeholder="Enter your password..."
-            />
-            <span className={styles.question}>?</span>
+          <img
+            className={styles.greetingKids}
+            src={greetingKids}
+            alt="A boy and a girl cheerfully waving at the user."
+          />
+          <div className={styles.guidelinesBox}>
+            <div className={styles.guidelinesCheckBox}>
+              <input type="checkbox" />
+              <label>
+                I pinky promise to follow the{" "}
+                <a href="/">community guidelines</a> and be awesome!
+              </label>
+            </div>
+            <p>
+              (Your secrets are safe with us! We're like ninjas for your privacy
+              - swift, silent and super secure.)
+            </p>
           </div>
-          <div className={styles.passBox}>
-            <input
-              className={styles.input}
-              type="password"
-              placeholder="Re-enter your password..."
-            />
-            <span className={styles.question}>?</span>
-          </div>
-        </div>
-        <img
-          className={styles.greetingKids}
-          src={greetingKids}
-          alt="A boy and a girl cheerfully waving at the user."
-        />
-        <div className={styles.guidelinesBox}>
-          <div className={styles.guidelinesCheckBox}>
-            <input type="checkbox" />
-            <label>
-              I pinky promise to follow the <a href="/">community guidelines</a>{" "}
-              and be awesome!
-            </label>
-          </div>
+          <button className={styles.btn}>Let's Do This!</button>
+        </form>
+        <div className={styles.recoveryBox}>
+          <p className={styles.recoveryTitle}>Already have an account?</p>
           <p>
-            (Your secrets are safe with us! We're like ninjas for your privacy -
-            swift, silent and super secure.)
+            Welcome back, hero! <a href="/">Click here to log-in.</a>
           </p>
         </div>
-        <button className={styles.btn}>Let's Do This!</button>
-      </form>
-      <div className={styles.recoveryBox}>
-        <p className={styles.recoveryTitle}>Already have an account?</p>
-        <p>
-          Welcome back, hero! <a href="/">Click here to log-in.</a>
-        </p>
-      </div>
-      {/* <div className={styles.recoveryBox}>
+        {/* <div className={styles.recoveryBox}>
           <p className={styles.recoveryTitle}>Forgot password?</p>
           <p>No worries - we've got your back!</p>
           <a href="/">Recover it here.</a>
         </div> */}
-    </article>
+      </article>
+    </div>
   );
 }
