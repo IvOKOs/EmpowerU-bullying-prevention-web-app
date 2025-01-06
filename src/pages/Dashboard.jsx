749ae08ceb96235ac3572447af5ce8b5ac847ed4
Ivo Kostadinov <ivaylo.kostadinov98@gmail.com>
2025-01-06 22:44:51 +0200

    Added the Dashboard page.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,8 +3,27 @@ import styles from "./Dashboard.module.css";
 export default function Dashboard() {
   return (
     <div className={styles.mainBox}>
-      <div className="container grid grid--3-cols">
-        <div></div>
+      <div className={`${styles.containerBox} grid grid--3-cols`}>
+        <div className={styles.mainInfo}>
+          <div className={styles.greetingBox}>
+            <img className={styles.profilePic} />
+            <header>Welcome back!</header>
+          </div>
+          <p className={styles.affirmation}>Affirmation</p>
+        </div>
+
+        <div className={styles.meterHelpBox}>
+          <div className={styles.meterBox}>
+            <div className={styles.meter}>
+              <div className={styles.meterFill}></div>
+            </div>
+            <span className={styles.meterTasks}>0/3</span>
+          </div>
+          <div className={styles.helpBox}>
+            <img />
+            <span>sgsgsgwgergr</span>
+          </div>
+        </div>
       </div>
     </div>
   );
