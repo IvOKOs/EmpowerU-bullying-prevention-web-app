931802d177616b29d0eb2842c455d6cf4a17ff8c
Ivo Kostadinov <ivaylo.kostadinov98@gmail.com>
2025-01-08 23:21:00 +0200

    Finished the helper with its bubble box + added more images

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,15 @@
 import styles from "./Dashboard.module.css";
+import cloud1 from "../assets/cloud1.png";
+import cloud2 from "../assets/cloud2.png";
+import meadows from "../assets/meadows.png";
+import nerdyGirl from "../assets/nerdy-girl.png";
+import peekingGirl from "../assets/peeking-girl.png";
+import { useState } from "react";
 
 export default function Dashboard() {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
+  // add border around the meter!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   return (
     <div className={styles.mainBox}>
       <div className={`${styles.containerBox} grid grid--3-cols`}>
@@ -19,9 +28,28 @@ export default function Dashboard() {
             </div>
             <span className={styles.meterTasks}>0/3</span>
           </div>
-          <div className={styles.helpBox}>
-            <img />
-            <span>sgsgsgwgergr</span>
+          <div
+            className={styles.helpBox}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseOver={() => setIsHovering(true)}
+            onMouseLeave={() => {
+              setIsHovered(false);
+              setIsHovering(false);
+            }}
+          >
+            <img
+              className={styles.nerdyGirl}
+              src={nerdyGirl}
+              alt="A nerdy-looking girl with glasses sitting on the ground and holding a book while looking at the user."
+            />
+            <span
+              className={`${styles.speechBubble} ${
+                isHovered ? styles.expandBounce : styles.shrink
+              } ${isHovering ? styles.hovering : ""}`}
+            >
+              Are you having trouble navigating? Click on me and I will try to
+              help!
+            </span>
           </div>
         </div>
       </div>
