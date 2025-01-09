21dd922277a9cda9c8472f6a1ae0cff77a3241d8
Ivo Kostadinov <ivaylo.kostadinov98@gmail.com>
2025-01-09 23:08:18 +0200

    Finished the Dashboard's layout.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,12 +7,38 @@ import peekingGirl from "../assets/peeking-girl.png";
 import { useState } from "react";
 
 export default function Dashboard() {
-  const [isHovered, setIsHovered] = useState(false);
-  const [isHovering, setIsHovering] = useState(false);
-  // add border around the meter!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  const [hover, setHover] = useState({
+    isHovered: false,
+    isHovering: false,
+  });
+
+  function handleHovered() {
+    setHover((prevHover) => {
+      const currHover = {
+        ...prevHover,
+        isHovered: true,
+      };
+      return currHover;
+    });
+  }
+
+  function handleHovering() {
+    setHover((prevHover) => {
+      const currHover = {
+        ...prevHover,
+        isHovering: true,
+      };
+      return currHover;
+    });
+  }
+
+  function handleMouseLeave() {
+    setHover({ isHovered: false, isHovering: false });
+  }
+
   return (
     <div className={styles.mainBox}>
-      <div className={`${styles.containerBox} grid grid--3-cols`}>
+      <div className={`${styles.containerBox} grid grid--3-cols grid--4-rows`}>
         <div className={styles.mainInfo}>
           <div className={styles.greetingBox}>
             <img className={styles.profilePic} />
@@ -30,12 +56,9 @@ export default function Dashboard() {
           </div>
           <div
             className={styles.helpBox}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseOver={() => setIsHovering(true)}
-            onMouseLeave={() => {
-              setIsHovered(false);
-              setIsHovering(false);
-            }}
+            onMouseEnter={handleHovered}
+            onMouseOver={handleHovering}
+            onMouseLeave={handleMouseLeave}
           >
             <img
               className={styles.nerdyGirl}
@@ -43,16 +66,63 @@ export default function Dashboard() {
               alt="A nerdy-looking girl with glasses sitting on the ground and holding a book while looking at the user."
             />
             <span
+              role="tooltip"
               className={`${styles.speechBubble} ${
-                isHovered ? styles.expandBounce : styles.shrink
-              } ${isHovering ? styles.hovering : ""}`}
+                hover.isHovered ? styles.expandBounce : styles.shrink
+              } ${hover.isHovering ? styles.hovering : ""}`}
             >
               Are you having trouble navigating? Click on me and I will try to
               help!
             </span>
           </div>
         </div>
+
+        {/* add shadow box on all boxes */}
+        <div className={styles.resourcesContainer}>
+          <span>My resources</span>
+          {/* here will be the 3 most recently saved resources */}
+          <div className={styles.resourcesBox}></div>
+          {/* onClick opens a modal of all saved resources */}
+        </div>
+
+        {/* onClick opens a modal which is the journal consisting of a textarea element */}
+        <div className={styles.journalBox}>
+          <span>My journal</span>
+          Thought of the day is... or emotion of the day is... you can write
+          about anything here...
+        </div>
+
+        <div className={styles.supportActionsBox}>
+          <span>My support actions</span>
+          <div className={styles.actionBox}>
+            You've helped 8 people by reporting incidents!
+          </div>
+        </div>
+
+        <div className={styles.factsContainer}>
+          <div className={styles.factsBox}>
+            <p className={styles.factsTitle}>Fun Fact!</p>
+            <div className={styles.fact}></div>
+            <div></div>
+          </div>
+        </div>
+
+        <div className={styles.activityContainer}>
+          <span>Recent activity</span>
+        </div>
+        <img
+          className={styles.peekingGirl}
+          src={peekingGirl}
+          alt="A young blonde girl with soft freckles and blue eyes, peeking curiously from behind a tree. Her wide, innocent eyes are focused directly on the viewer, radiating a sense of wonder and playfulness."
+        />
+        <img className={styles.cloud1} src={cloud1} alt="A cloud." />
+        <img className={styles.cloud2} src={cloud2} alt="A cloud." />
       </div>
+      <img
+        className={styles.meadows}
+        src={meadows}
+        alt="A serene and expansive meadow with vibrant green grass, scattered with colorful wildflowers like daisies, poppies, and lavender."
+      />
     </div>
   );
 }
